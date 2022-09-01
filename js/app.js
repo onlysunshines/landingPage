56cacbd9505f38e562ef9a44b0da8f804b0c52ea
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2022-09-01 17:49:10 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,9 +26,6 @@
 
 const sectionElements = document.getElementsByTagName('section');
 
-//const clickLi = document.getElementsByTagName('li');
-
-
 /*
  * Begin Main Functions
 */
@@ -49,31 +46,6 @@ function buildNav() {
         document.getElementById('navbar__list').append(navbar);
     }
 };
-
-/*
-let buildNav = () => {
-    sectionElements.forEach(section => {
-        let navbar = document.createElement('li');
-        navbar.className = 'menu__link';
-        navbar.innerHTML = `<a href='#${sectionElement.id}'>${sectionElements.dataset.nav}</a>`;
-    });
-    navbar.onclick = scrollTo(sectionElement.id);
-
-    document.getElementById('navbar__list').append(navbar);
-}
-*/
-
-/*
-sectionElements.forEach(section => {
-    let navbar = document.createElement('li');
-    navbar.className = 'menu__link';
-    navbar.innerHTML = `<a href='#${sectionElement.id}'>${sectionElements.dataset.nav}</a>`;
-    
-    navbar.onclick = scrollTO(sectionElement.id);
-
-    document.getElementById('navbar__list').append(navbar);
-});
-*/
 
 // Add class 'active' to section when near top of viewport
 
@@ -97,179 +69,15 @@ function isActive() {
     }
 }
 
-/*
+function percentInViewport(element, percentVisible) {
+    let rect = element.getBoundingClientRect(),
+        windowHeight = (window.innerHeight || document.documentElement.clientHeight);
 
-for (let sectionElement of sectionElements) {
-        if (sectionElement.getBoundingClientRect().top < 100 ) {
-            sectionElement.className = 'your-active-class';
-        } else {
-            sectionElement.classList.remove('your-active-class');
-        }
-    }
-    
-    for (let sectionElement of sectionElements) {
-        if (sectionElement.className === 'your-active-class') {
-            console.log('my work here is done');
-        } else {
-            sectionElement.classList.remove('your-active-class');
-        }
-    };
-
-function isInViewport() {
-
-    for (let sectionElement of sectionElements) {
-        let distance = sectionElement.getBoundingClientRec();
-        if (distance.top >= 0 &&
-            distance.left >= 0 &&
-            distance.bottom <= (window.innerWidth || document.documentElement.clientHeight) &&
-            distance.right <= (window.innerWidth || document.documentElement.clientWidth)) {
-                sectionElement.className = 'your-active-class';
-            } else {
-                sectionElement.className = '';
-            }
-            
-    }
-
+    return !(
+        Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percentVisible ||
+        Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+    )
 };
-
-
-let isInViewport = function (e) {
-    let distance = e.getBoundingClientRec();
-    return (
-        distance.top >= 0 &&
-        distance.left >= 0 &&
-        distance.bottom <= (window.innerWidth || document.documentElement.clientHeight) &&
-        distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-
-sections.forEach(section => {
-  section.addEventListener('click', () => {
-
-    // I loop again to remove the 'section--active' from all others sections
-    // To avoid confusion, I use 'el' as a name for each section in sections
-    sections.forEach(el => el.classList.remove('section--active'))
-
-    // Then I add the 'section--active' to the clicked element
-    section.classList.add('section--active')
-  })
-})
-
-function removeClass() {
-    let activeElements = document.getElementsByClassName('your-active-class')
-    for (element of activeElements) {
-       if (element !== sectionElements.getBoundingClientRect().top < 100){
-        sectionElements.className = 'your-active-class';
-       } else {
-        sectionElements.classList.remove('your-active-class')
-    }
- }
-}
-
-
-let myElement = document.getElementById('my-element');
-let bounding = myElement.getBoundingClientRect();
-
-function elementInViewport() {
-
-    let bounding = myElement.getBoundingClientRect();
-
-    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-
-        console.log('Element is in the viewport!');
-    } else {
-
-        console.log('Element is NOT in the viewport!');
-    }
-}
-
-
-let myElement = document.getElementById('my-element');
-let bounding = myElement.getBoundingClientRect();
-
-if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= window.innerWidth && bounding.bottom <= window.innerHeight) {
-
-    console.log('Element is in the viewport!');
-} else {
-
-    console.log('Element is NOT in the viewport!');
-}
-
-
-
-function isActive() {
-        let a = sectionElements;
-        for (let i = 0, length = a.length; i < length; i++) {
-            a[i].onclick = function() {
-                let b = document.querySelector('.your-active-class li');
-                if (b) b.classList.remove('your-active-class');
-                this.parentNode.classList.add('your-active-class');
-              };
-        }
-    }
-
-
-*/
-
-/*
-function isActive() {
-    clickLi.forEach(clickLi => {
-        clickLi.addEventListener('click', function() {
-            clickLi.forEach(li.classList.remove('your-active-class'));
-            this.classList.add('your-active-class')
-        })
-    })
-}
-*/
-
-/*
-function isActive() {
-
-    for (let i = 0; i < clickLi.length; i++) {
-        clickLi[i].addEventListener('click', function() {
-            let current = document.getElementsByClassName('your-active-class');
-            current[0].className = current[0].className.replace('your-active-class', '');
-            this.className += 'your-active-class';
-        });
-    }
-
-}
-*/
-
-/*
-
-function isActive() {
-
-    for (let sectionElement of sectionElements) {
-            if (sectionElement.getBoundingClientRect().top < 100) {
-                sectionElement.className = "your-active-class";
-            } else {
-                sectionElement.className = "";
-            }
-        }
-}
-
-let isActive = () => {
-    sectionElements.forEach(section => {
-        if (section.getBoundingClientRect().top < 100) {
-            section.className = 'your-active-class';
-        } else {
-            section.className = '';
-        }
-    })
-};
-*/
-
-/*
-sectionElements.forEach(section => {
-    if (section.getBoundingClientRect().top < 100) {
-        section.className = 'your-active-class';
-    } else {
-        section.className = '';
-    }
-});
-*/
-
 
 // Scroll to anchor ID using scrollTO event
 
@@ -283,18 +91,6 @@ function scrollTO(e) {
     }
 }
 
-/*
-let scrollTO = (e) => {
-    if (e && e.nodName === 'li') {
-        let a = e.getElementsByTagName('a');
-        let navElement = a.href;
-        document.getElementById(navElement).scrollIntoView();
-        // active function called here
-        isActive;
-    }
-}
-*/
-
 /**
  * End Main Functions
  * Begin Events
@@ -306,15 +102,8 @@ window.onload = buildNav;
 
 // Scroll to section on link click
 
-
-/*
-clickLi.addEventListener('click', function() {
-    console.log('this has been clicked')
-});
-*/
-// 
-//navbar.onclick = scrollTO(sectionElement.id);
-// document.getElementsByTagName("li").addEventListener("click", scrollTO);
+// this line from buildNav is accomplishing this
+// navbar.onclick = scrollTO(sectionElement.id);
 
 // Set sections as active
 
